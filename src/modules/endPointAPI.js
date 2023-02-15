2786210a7bd062826b1b2c5c389aabd9bdd7d5f0
JakariaJishan <jakariajishan987@gmail.com>
2023-02-16 03:00:42 +0600

    Add comment popup.

--- src/modules/endPointAPI.js
+++ src/modules/endPointAPI.js
@@ -1,4 +1,4 @@
-import apiKey from './apiKey.js';
+import { apiKey } from './apiKey.js';
 
 const getMovieList = async () => {
   const url = `https://api.themoviedb.org/3/list/8240326?api_key=${apiKey}&language=en-US`;
@@ -7,4 +7,12 @@ const getMovieList = async () => {
   return movieList;
 };
 
-export default getMovieList;
+const getSingleMovie = async (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`;
+  const response = await fetch(url);
+  const singleMovie = await response.json();
+  return singleMovie;
+}
+
+export { getMovieList, getSingleMovie };
+
--- src/modules/render.js
+++ src/modules/render.js
@@ -1,26 +1,51 @@
-import getMovieList from './endPointAPI.js';
-import moreInfo from '../assets/images/more-info.png';
-import heart from '../assets/images/heart.png';
+import heart from "../assets/images/heart.png";
+import moreInfo from "../assets/images/more-info.png";
+import { getMovieList, getSingleMovie } from "./endPointAPI.js";
 
 const renderMovie = (movieList) => {
-  document.querySelector('section.movie');
-  let aux = '';
+  document.querySelector("section.movie");
+  let aux = "";
   movieList.forEach((movie) => {
-    aux += `<div class="movie">
+    aux += `<div class="movie" >
             <img class="movie" src="https://image.tmdb.org/t/p/w300/${movie.poster_path}" alt="">
             <div class="button-container">
               <img class="btn-more-info" src="${moreInfo}" alt="">
               <img class="btn-like" src="${heart}" alt="">
             </div>
+            <button onclick="commentPupupHandler(${movie.id})">comments</button>
           </div>`;
   });
-  document.querySelector('section.movie').innerHTML = aux;
+  document.querySelector("section.movie").innerHTML = aux;
+  document.getElementById("popup").style.display = "none";
 };
 
 const renderMovieList = () => {
   getMovieList().then((response) => {
     renderMovie(response.items);
   });
+};
+
+window.commentPupupHandler = async (id) => {
+  document.getElementById("popup").style.display = "block";
+  
+  getSingleMovie(id).then((data) => {
+    document.getElementById("popup-details").innerHTML = `
+    <button onclick="closePopup()">x</button>
+    <div class="popup-contents">
+      <div class="popup-comment">
+        <img class="popup-img" src="https://image.tmdb.org/t/p/w300/${data.poster_path}" alt="">
+        <p class="title">${data.title}</p>
+        <div class="more-info">
+          <p>popularity : ${data.popularity}</p>
+          <p>language: ${data.original_language}</p>
+        </div>
+      </div>
+    </div>`;
+  });
+};
+
+window.closePopup = () => {
+  document.getElementById("popup").style.display = "none";
 };
 
 export default renderMovieList;
